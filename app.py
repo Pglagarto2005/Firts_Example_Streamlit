a22ce98cdda3717fbcb9c94ab0c2b50d0e2a93aa
Pglagarto2005 <santiblo05@gmail.com>
2026-02-26 10:03:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from PIL import Image
 
 st.title("BANANA ROTATE")
+
+st.header("RETURN TO MONKEY")
+st.write("Donkey kong")
+image = Image.open("banana.gif")
+st.image(image, caption= "Banana") 
