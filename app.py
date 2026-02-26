faec23804e6c9fdba4133ccc2d8ec7cd5bd39873
Pglagarto2005 <santiblo05@gmail.com>
2026-02-26 10:26:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,17 @@ st.image(image, caption= "Banana")
 
 texto = st.text_input("write name", "")
 st.write("Your monkey name is ", texto)
+
+
+st.subheader("Monkey divide")
+
+coll, col2 = st.columns(2)
+
+with coll:
+  st.subheader("Monkey team 1")
+  st.write("Donkey Kong")
+  image2 = Image.open("DK.png")
+  st.image(image2, caption= "DK") 
+  resp  = st.checkbox(TEAM DK)
+  if resp:
+    st.write("OK!")
