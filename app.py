0afcae6dbc6daa0fc5c5e335e24885dda7752194
Pglagarto2005 <santiblo05@gmail.com>
2026-02-26 10:16:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ st.header("RETURN TO MONKEY")
 st.write("Donkey kong")
 image = Image.open("banana.png")
 st.image(image, caption= "Banana") 
+
+
+texto = st.text_input("write name", "...")
+st.write("Monkey name", texto)
